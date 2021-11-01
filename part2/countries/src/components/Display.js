6ae3b60350b5355601509777b77df7f60bca69d7
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2021-11-01 19:49:04 +0200

    ex 2.13

--- part2/countries/src/components/Display.js
+++ part2/countries/src/components/Display.js
@@ -1,33 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from './Button'
+import Info from './Info'
 
 const Display = ({ filter, countries }) => {
+  const [displayCountry, setDisplayCountry] = useState(false)
+
   // Add index for the countries
   countries.forEach((obj, i) => obj.index = i)
 
   const filtCountries = countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
   const len = filtCountries.length
+  
   if (!filter) { // no filter
     return <p>Find countries by name</p>
   } else if (len > 10) { 
     return <p>Too many matches, specify another filter</p>
-  } else if (len > 1) {
-    return filtCountries.map(country => <p key={country.index}>{country.name.common}</p>)
-  } else if (len === 1) { // display information for the country
-    const country = filtCountries[0]
-    const languages = Object.keys(country.languages).map(key => country.languages[key])
-
+  } else if (len > 1 && !displayCountry) {
+    return filtCountries.map(country => {
+      return (
+        <div key={country.index}>
+          <p>{country.name.common}</p>
+          <Button text="show" onClick={() => setDisplayCountry(country)} />
+        </div>
+      )
+    })
+  } else if (displayCountry) {
     return (
       <div>
-        <h1>{country.name.common}</h1>
-        <p>Capital: {country.capital[0]}</p>
-        <p>Land area: {country.area} km<sup>2</sup></p>
-        <h2>Languages</h2>
-        <ul>
-          {languages.map((lang, i) => <li key={i}>{lang}</li>)}
-        </ul>
-        <img src={country.flags.png} alt="flag" />
+        <Info country={displayCountry} />
+        <Button text="show all" onClick={() => setDisplayCountry(false)} />
       </div>
-    )
+    ) 
+  } else if (len === 1) { // display information for the country
+    return <Info country={filtCountries[0]} />
   } else {
     return <p>No country found</p>
   }
---
+++ part2/countries/src/components/Info.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Info = props => {
+  const country = props.country
+  const languages = Object.keys(country.languages).map(key => country.languages[key])
+
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital[0]}</p>
+      <p>Land area: {country.area} km<sup>2</sup></p>
+      <h2>Languages</h2>
+      <ul>
+        {languages.map((lang, i) => <li key={i}>{lang}</li>)}
+      </ul>
+      <img src={country.flags.png} alt="flag" />
+    </div>
+  )
+}
+
+export default Info
