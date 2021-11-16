1e483223807df48ce8a034169c8cbdb818f51157
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2021-11-16 13:23:56 +0200

    ex 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,6 +4,20 @@ const totalLikes = blogs => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-module.exports = {
-  dummy, totalLikes
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return null
+
+  let maxLikes = -1
+  let maxIdx = -1
+
+  blogs.forEach((blog, i) => {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes
+      maxIdx = i
+    } 
+  })
+
+  return blogs[maxIdx]
 }
+
+module.exports = { dummy, totalLikes, favoriteBlog }
