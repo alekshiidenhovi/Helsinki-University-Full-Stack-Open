b400a2e95f21dde05716179d4395d0929117ed33
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2022-01-06 13:38:47 +0200

    ex 6.16

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { newAnecdote } from '../reducers/anecdoteReducer'
 import { newNotification, hideNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -11,9 +10,8 @@ const AnecdoteForm = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    
-    const anec = await anecdoteService.createAnecdote(content) 
-    dispatch(newAnecdote(anec))
+
+    dispatch(newAnecdote(content))
     dispatch(newNotification(content, 'created'))
     setTimeout(() => dispatch(hideNotification()), 5000)
   }
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -18,7 +18,7 @@ const anecdoteReducer = (state = [], action) => {
       const votedAnecdote = state.find(anecdote => anecdote.id === id)
       const changedAnecdote = { ...votedAnecdote, votes: votedAnecdote.votes + 1 }
       return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
-    case 'NEW_ANECDOTES':
+    case 'NEW_ANECDOTE':
       return [...state, action.data]
     case 'INIT_ANECDOTES':
       return action.data
@@ -34,10 +34,13 @@ export const vote = id => {
   }
 }
 
-export const newAnecdote = data => {
-  return { 
-    type: 'NEW_ANECDOTE', 
-    data
+export const newAnecdote = content => {
+  return async dispatch => {
+    const anecdote = await anecdoteService.createAnecdote(content)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: anecdote
+    })
   }
 }
 
